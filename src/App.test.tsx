639aab5874377f787e6e56f4d13bc23e5bf63d76
adamrturman <adamrturman@gmail.com>
2021-04-06 14:29:47 -0500

    added unit test for adding todo and clearing input

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const addAToDo = (input: HTMLElement, text: string, button: HTMLElement) => {
+    fireEvent.change(input, { target: { value: text }});
+    fireEvent.click(button);
+};
+
+test('adding new todos to the list and input clears', () => {
+    const { getByText, getByLabelText, queryByText } = render(<App />);
+    const input = getByLabelText("Add a new todo");
+    const addButton = getByText("Click to Add");
+    const inputValue = input.innerHTML;
+
+    addAToDo(input, "get bread", addButton);
+    const getBreadText = queryByText("get bread");
+
+    expect(getBreadText).toBeInTheDocument();
+    expect(inputValue).toBe("");
 });
